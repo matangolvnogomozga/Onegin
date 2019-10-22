b30a31392c8d9107182adccdffd7a2d0f476a816
Игорь Оберман <56135465+matangolvnogomozga@users.noreply.github.com>
2019-10-23 00:48:55 +0300

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
+#include <math.h>
+
+const long MAXSYMB = 10e6; ///max number of symbols in file
+const int MAXLINES = 10e6; ///max number of lines in file
+
+/** Struct "line" contains pointer to the start of the line end it's length **/
+struct line
+{
+    char* pointer;
+    int len;
+};
+
+long CountSymbols(FILE *f);
+int CountLines(char text[], int max);
+char* FileToArray(FILE* file, long numsymb);
+line* ArrayToStruct(char text[], int numline, long numsymb);
+int Compar(const void* a, const void* b);
+char* PunctOut(char* str);
+int Min(int a, int b);
+void StructToFile(line* text, int number, FILE* file);
+int ComparEnd(const void* a, const void* b);
+void ArrayToFile(char text[], long numsymb, FILE* file);
